2a746d3310b27e175d99383b8487d249e9de6fe3
toffeechay <138518225+stefaniesinner@users.noreply.github.com>
2023-07-05 20:24:42 +0200

    Add variables to use the ladder

--- Assets/LadderMovement.cs
+++ Assets/LadderMovement.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class LadderMovement : MonoBehaviour
 {
+    // to use the ladder
+    private float moveVertical;
+    private float speed = 8f;
+    private bool isLadder; // to know if the player is standing on the ladder
+    private bool isClimbing;
+
     // Start is called before the first frame update
     void Start()
     {
