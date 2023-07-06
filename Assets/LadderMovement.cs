d5e14470f4153fa35c0074fe27f00ee8b405f36e
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-07-06 08:27:38 +0200

    Add methods to LadderMovement

--- Assets/LadderMovement.cs
+++ Assets/LadderMovement.cs
@@ -4,15 +4,60 @@ using UnityEngine;
 
 public class LadderMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float moveVertical;
+    private float speed = 8f;
+    private bool isLadder; // to know if the player is standing next to the ladder
+    private bool isClimbing;
+
+    [SerializeField] private Rigidbody2D rb; // to reference the players rigidbody
 
     // Update is called once per frame
     void Update()
     {
-        
+        moveVertical = Input.GetAxis("Vertical");
+
+        if (isLadder && Mathf.Abs(moveVertical) > 0f)
+        {
+            isClimbing = true;
+        }
+    }
+
+    /*
+     * Disable gravity and move the player.
+     */
+    private void FixedUpdate()
+    {
+        if (isClimbing)
+        {
+            rb.gravityScale = 0f;
+            rb.velocity = new Vector2(rb.velocity.x, moveVertical * speed);
+        }
+        else
+        {
+            rb.gravityScale = 5f; // default speed of the player
+        }
+    }
+
+    /*
+     * Check if the player is standing to the ladder.
+     */
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ladder"))
+        {
+            isLadder = true;
+        }
+    }
+
+    /*
+     * Check if the player is standing to the ladder yet.
+     */
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ladder"))
+        {
+            isLadder = false;
+            isClimbing = false;
+        }
     }
 }
