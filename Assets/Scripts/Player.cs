940d9cf8adf0c784b9685f8d5b1214c6e291d7e8
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-07-07 14:36:01 +0200

    Add documentary to Player Script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// <c>Player</c> contains all information necessary for the user to control and interact with the game character (the librarian).
+/// </summary>
 public class Player : MonoBehaviour
 {
 
@@ -9,37 +12,30 @@ public class Player : MonoBehaviour
 
     public bool isGrounded = false;
     public bool isMoving = false;
+    private bool isTouchingLadder;
+    private bool isClimbing;
 
-    // to control the movements of the player
     public float speed = 5f;
     public float jumpForce = 3;
     public float moveHorizontal;
     public float moveVertical;
 
-    public LayerMask groundLayer; // to know if and which floor the player is touching
+    public LayerMask groundLayer; // To know if and which floor the player is touching
     public float radius = 0.3f;
     public float groundRayDist = 0.5f;
-
-    // to use the ladder
-    private bool isLadder; // to know if the player is standing next to the ladder
-    private bool isClimbing;
 
     private Rigidbody2D rb;
     private Animator anim;
     private SpriteRenderer spr;
 
-
-    /*
-     * Implements the player.
-     */
+    /// <summary>
+    /// Construct the players' game character.
+    /// </summary>
     private void Awake()
     {
         player = this;
     }
 
-    /*
-     * This method is called before updating the first frame.
-     */
     private void Start() 
     {
         rb = GetComponent<Rigidbody2D>();
@@ -47,9 +43,6 @@ public class Player : MonoBehaviour
         spr = GetComponent<SpriteRenderer>();
     }
 
-    /*
-     * Update the player once per frame by changing the sprites.
-     */
     private void Update()
     {
         moveHorizontal = Input.GetAxisRaw("Horizontal");
@@ -60,9 +53,11 @@ public class Player : MonoBehaviour
         isGrounded = Physics2D.CircleCast(transform.position, radius, Vector3.down, groundRayDist, groundLayer);
 
         if (Input.GetKeyDown(KeyCode.Space))
+        {
             Jump();
+        }
 
-        if (isLadder && Mathf.Abs(moveVertical) > 0f)
+        if (isTouchingLadder && Mathf.Abs(moveVertical) > 0f)
         {
             isClimbing = true;
         }
@@ -73,9 +68,6 @@ public class Player : MonoBehaviour
         Flip(moveHorizontal);
     }
 
-    /*
-     * Moves the player.
-     */
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(moveHorizontal * speed, rb.velocity.y);
@@ -88,62 +80,61 @@ public class Player : MonoBehaviour
         } 
         else
         {
-            rb.gravityScale = 3; // have an effect on players jump
+            rb.gravityScale = 3;
         }
     }
 
-    /*
-     * ...
-     */
+    /// <summary>
+    /// Allow the player to jump and check if he is on the ground.
+    /// </summary>
     private void Jump()
     {
-        if (!isGrounded) return;
+        if (!isGrounded)
+        {
+            return;
+        }
 
         rb.velocity = Vector2.up * jumpForce;
     }
 
-    /*
-     * Flips the sprites of our player according to where he moves (left or right).
-     */
-    private void Flip(float _xValue)
+    /// <summary>
+    /// Flip the sprites of the player according to his movements. If the player moves to the opposite direction (to the left), 
+    /// flip the player's scale. Otherwise, maintain the original scale without changes.
+    /// </summary>
+    /// <param name="xDirection">The direction on the x-coordinate.</param>
+    private void Flip(float xDirection)
     {
-        Vector3 theScale = transform.localScale;
+        Vector3 playerScale = transform.localScale;
 
-        if (_xValue < 0)
-            theScale.x = Mathf.Abs(theScale.x) * -1;
-        else
-        if (_xValue > 0)
-            theScale.x = Mathf.Abs(theScale.x);
+        if (xDirection < 0)
+        {
+            playerScale.x = Mathf.Abs(playerScale.x) * -1;
+        }
+        else if (xDirection > 0)
+        {
+            playerScale.x = Mathf.Abs(playerScale.x);
+        }
 
-        transform.localScale = theScale;
+        transform.localScale = playerScale;
     }
 
-    /*
-     * Check if the player is standing to the ladder.
-     */
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Ladder"))
         {
-            isLadder = true;
+            isTouchingLadder = true;
         }
     }
 
-    /*
-     * Check if the player is standing to the ladder yet.
-     */
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag("Ladder"))
         {
-            isLadder = false;
+            isTouchingLadder = false;
             isClimbing = false;
         }
     }
 
-    /*
-     * ...
-     */
     private void OnDestroy()
     {
         player = null;
