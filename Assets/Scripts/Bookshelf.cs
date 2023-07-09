d1ea06fa3306716ad4a871038beaf2301f13636f
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-07-09 08:17:22 +0200

    Update Bookshelf.cs

--- Assets/Scripts/Bookshelf.cs
+++ Assets/Scripts/Bookshelf.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Bookshelf : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public List<GameObject> books = new List<GameObject>();
+    public GameObject window;
 
-    // Update is called once per frame
-    void Update()
+    public void PickUp(GameObject book)
     {
-        
+        books.Add(book);
     }
 }
