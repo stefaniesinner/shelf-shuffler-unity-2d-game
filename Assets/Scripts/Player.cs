1c11020dbd38f4030edffc654c858ea2258c48dd
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-07-16 12:29:51 +0200

    Revert "Update Player.cs"
    This reverts commit b568194ec609ab9ebf6b80f7a2fcae9c6be2b458.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,20 +4,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
     public static Player player;
 
-    private bool isMoving;
-    private bool isGrounded;
-    private bool isTouchingLadder;
-    private bool isClimbing;
-    private bool vertical;
+    public bool isGrounded = false;
+    public bool isMoving = false;
+    private bool isTouchingLadder = false;
+    private bool isClimbing = false;
+    private bool vertical = false;
 
-    private float moveHorizontal;
-    private float moveVertical;
-    private float speed = 5f;
-    private float jumpingPower = 3;
+    public float moveHorizontal;
+    public float moveVertical;
+    public float speed = 5f;
+    public float jumpForce = 3;
 
-    public LayerMask groundLayer; // To know if and which ground the player is touching
+    // To know if and which floor the player is touching
+    public LayerMask groundLayer;
     public float radius = 0.3f;
     public float groundRayDist = 0.5f;
 
@@ -28,52 +30,14 @@ public class Player : MonoBehaviour
 
     private void Awake()
     {
-        player = GetComponent<Player>();
+        player = this;
     }
 
-    private void Start()
+    private void Start() 
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         spr = GetComponent<SpriteRenderer>();
-    }
-
-    private void Jump()
-    {
-        if (!isGrounded)
-        {
-            return;
-        }
-
-        rb.velocity = Vector2.up * jumpingPower;
-    }
-
-    private void FlipSprite(float xDirection)
-    {
-        Vector3 scaleOfPlayer = transform.localScale;
-
-        // Moves to the left
-        if (xDirection < 0)
-        {
-            // Flip the player's scale
-            scaleOfPlayer.x = Mathf.Abs(scaleOfPlayer.x) * -1;
-        }
-        // Moves to the right
-        else if (xDirection > 0)
-        {
-            // Maintain the original scale without changes
-            scaleOfPlayer.x = Mathf.Abs(scaleOfPlayer.x);
-        }
-
-        transform.localScale = scaleOfPlayer;
-    }
-
-    private void AnimatePlayer()
-    {
-        anim.SetBool("isMoving", isMoving);
-        anim.SetBool("isGrounded", isGrounded);
-        anim.SetBool("isClimbing", isClimbing);
-        anim.SetBool("isVertical", vertical);
     }
 
     private void Update()
@@ -85,7 +49,6 @@ public class Player : MonoBehaviour
 
         isGrounded = Physics2D.CircleCast(transform.position, radius, Vector3.down, groundRayDist, groundLayer);
 
-
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Jump();
@@ -95,15 +58,17 @@ public class Player : MonoBehaviour
         {
             isClimbing = true;
             vertical = true;
-        }
-        else if (Mathf.Abs(moveVertical) == 0)
+        } else if ( Mathf.Abs(moveVertical) == 0)
         {
             vertical = false;
         }
 
-        AnimatePlayer();
+        anim.SetBool("isMoving", isMoving);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetBool("isClimbing", isClimbing);
+        anim.SetBool("isVertical", vertical);
 
-        FlipSprite(moveHorizontal);
+        Flip(moveHorizontal);
     }
 
     private void FixedUpdate()
@@ -115,11 +80,37 @@ public class Player : MonoBehaviour
         {
             rb.gravityScale = 0f;
             rb.velocity = new Vector2(rb.velocity.x, moveVertical * speed);
-        }
+        } 
         else
         {
             rb.gravityScale = 3;
         }
+    }
+
+    private void Jump()
+    {
+        if (!isGrounded)
+        {
+            return;
+        }
+
+        rb.velocity = Vector2.up * jumpForce;
+    }
+
+    private void Flip(float xDirection)
+    {
+        Vector3 playerScale = transform.localScale;
+
+        if (xDirection < 0)
+        {
+            playerScale.x = Mathf.Abs(playerScale.x) * -1;
+        }
+        else if (xDirection > 0)
+        {
+            playerScale.x = Mathf.Abs(playerScale.x);
+        }
+
+        transform.localScale = playerScale;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -143,4 +134,5 @@ public class Player : MonoBehaviour
     {
         player = null;
     }
+
 }
