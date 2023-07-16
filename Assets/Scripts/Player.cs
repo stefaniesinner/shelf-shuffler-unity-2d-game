af4f76b3f8c599ba7bb80665ec86bba96357c3b4
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-07-16 12:33:06 +0200

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -30,61 +30,14 @@ public class Player : MonoBehaviour
 
     private void Awake()
     {
-        player = this;
+        player = GetComponent<Player>();
     }
 
-    private void Start() 
+    private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         spr = GetComponent<SpriteRenderer>();
-    }
-
-    private void Update()
-    {
-        moveHorizontal = Input.GetAxisRaw("Horizontal");
-        moveVertical = Input.GetAxisRaw("Vertical");
-
-        isMoving = (moveHorizontal != 0);
-
-        isGrounded = Physics2D.CircleCast(transform.position, radius, Vector3.down, groundRayDist, groundLayer);
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Jump();
-        }
-
-        if (isTouchingLadder && Mathf.Abs(moveVertical) > 0f)
-        {
-            isClimbing = true;
-            vertical = true;
-        } else if ( Mathf.Abs(moveVertical) == 0)
-        {
-            vertical = false;
-        }
-
-        anim.SetBool("isMoving", isMoving);
-        anim.SetBool("isGrounded", isGrounded);
-        anim.SetBool("isClimbing", isClimbing);
-        anim.SetBool("isVertical", vertical);
-
-        Flip(moveHorizontal);
-    }
-
-    private void FixedUpdate()
-    {
-        rb.velocity = new Vector2(moveHorizontal * speed, rb.velocity.y);
-
-
-        if (isClimbing)
-        {
-            rb.gravityScale = 0f;
-            rb.velocity = new Vector2(rb.velocity.x, moveVertical * speed);
-        } 
-        else
-        {
-            rb.gravityScale = 3;
-        }
     }
 
     private void Jump()
@@ -97,20 +50,78 @@ public class Player : MonoBehaviour
         rb.velocity = Vector2.up * jumpForce;
     }
 
-    private void Flip(float xDirection)
+    private void FlipSprite(float xDirection)
     {
-        Vector3 playerScale = transform.localScale;
+        Vector3 scaleOfPlayer = transform.localScale;
 
+        // Moves to the left
         if (xDirection < 0)
         {
-            playerScale.x = Mathf.Abs(playerScale.x) * -1;
+            // Flip the player's scale
+            scaleOfPlayer.x = Mathf.Abs(scaleOfPlayer.x) * -1;
         }
+        // Moves to the right
         else if (xDirection > 0)
         {
-            playerScale.x = Mathf.Abs(playerScale.x);
+            // Maintain the original scale without changes
+            scaleOfPlayer.x = Mathf.Abs(scaleOfPlayer.x);
         }
 
-        transform.localScale = playerScale;
+        transform.localScale = scaleOfPlayer;
+    }
+
+    private void AnimatePlayer()
+    {
+        anim.SetBool("isMoving", isMoving);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetBool("isClimbing", isClimbing);
+        anim.SetBool("isVertical", vertical);
+    }
+
+    private void Update()
+    {
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        moveVertical = Input.GetAxisRaw("Vertical");
+
+        isMoving = (moveHorizontal != 0);
+
+        isGrounded = Physics2D.CircleCast(transform.position, radius, Vector3.down, groundRayDist, groundLayer);
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+
+        if (isTouchingLadder && Mathf.Abs(moveVertical) > 0f)
+        {
+            isClimbing = true;
+            vertical = true;
+        }
+        else if (Mathf.Abs(moveVertical) == 0)
+        {
+            vertical = false;
+        }
+
+        AnimatePlayer();
+
+        FlipSprite(moveHorizontal);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveHorizontal * speed, rb.velocity.y);
+
+
+        if (isClimbing)
+        {
+            rb.gravityScale = 0f;
+            rb.velocity = new Vector2(rb.velocity.x, moveVertical * speed);
+        }
+        else
+        {
+            rb.gravityScale = 3;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
