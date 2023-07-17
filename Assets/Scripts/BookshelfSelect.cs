437511cd24e175905c0c6904208703d92cb42791
Gaman <gaman2201@yahoo.de>
2023-07-17 23:41:50 +0200

    added books and highligh

---
+++ Assets/Scripts/BookshelfSelect.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookshelfSelect : MonoBehaviour
+{
+    //Get Bookshelf ParentObject
+    //Variable bool selectedField
+    //List with all books, order important
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Instantiate every Books at right position
+        //make it an child of bookshelf
+        //store it in List
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if the collider in bookshelf is finding something
+        //set selectedField to first books ->get it from List
+    }
+
+    void loadBooks()
+    {
+        //2 For Loop according to Shelf position
+        //Instantiate(); TODO How to get prefab object?
+    }
+}
