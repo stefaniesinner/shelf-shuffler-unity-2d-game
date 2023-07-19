eabe7203e4f4cfce33cd5c19b19da9f88cdf074f
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-07-19 11:38:10 +0200

    Update

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Item : MonoBehaviour
+{
+    public enum InteractionType { NONE, PickUp, Examine, GrabDrop }
+    public InteractionType type;
+
+    private void Reset()
+    {
+        GetComponent<Collider2D>().isTrigger = true;
+        gameObject.layer = 10;
+    }
+
+    public void Interact()
+    {
+        switch (type) 
+        {
+            case InteractionType.PickUp:
+                Debug.Log("PICK UP");
+                break;
+            case InteractionType.Examine:
+                Debug.Log("EXAMINE");
+                break;
+            case InteractionType.GrabDrop:
+                Debug.Log("GRAB");
+                break;
+            default:
+                Debug.Log("NULL ITEM");
+                break;
+        }
+    }
+}
