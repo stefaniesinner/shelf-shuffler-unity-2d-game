c66e10d87d562bb5d2814adad13d21c3b9984539
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-07-19 10:34:48 +0200

    Add InteractionSystem.cs

---
+++ Assets/Scripts/InteractionSystem.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionSystem : MonoBehaviour
+{
+    // Detection Point
+    public Transform detectionPoint;
+    // Detection Radius
+    private const float detectionRadius = 0.2f;
+    // Detection Layer
+    public LayerMask detectionLayer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (DetectObject())
+        {
+            if (InteractInput())
+            {
+                Debug.Log("INTERACT");
+            }
+        }
+    }
+
+    bool InteractInput()
+    {
+        return Input.GetKeyDown(KeyCode.E);
+    }
+
+    bool DetectObject()
+    {
+        return Physics2D.OverlapCircle(detectionPoint.position, detectionRadius, detectionLayer);
+    }
+}
