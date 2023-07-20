a2b9e0f37983cd6738ce2b867b4e85f55000de44
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-07-20 21:06:39 +0200

    Update BookshelfController.cs

--- Assets/Scripts/Bookshelf/BookshelfController.cs
+++ Assets/Scripts/Bookshelf/BookshelfController.cs
@@ -8,25 +8,51 @@ public class BookshelfController : MonoBehaviour
     [SerializeField]
     private GameObject bookshelfWindow;
     [SerializeField]
-    private GameObject indicator; // To inform the player which button to press to open the window
+    private GameObject buttonIndicator; // To inform the player which button to press to open the window
+    [SerializeField]
+    private KeyCode openKey = KeyCode.E;
+
+    private bool isDetectingPlayer;
 
     private void Start()
     {
-        
+        ShowBookshelfWindow(false);
+        ShowButtonIndicator(false);
     }
 
     private void Update()
     {
-        
+        if (Input.GetKeyUp(openKey) && isDetectingPlayer) // + when player is next to bookshelf!!
+        {
+            ShowBookshelfWindow(true);
+        }
     }
 
-    private void OpenBookshelfWindow()
+    private void ShowBookshelfWindow(bool isShowing)
     {
-        
+        bookshelfWindow.SetActive(isShowing);
     }
 
-    private void CloseBookshelfWindow()
+    private void ShowButtonIndicator(bool isShowing)
     {
+        buttonIndicator.SetActive(isShowing);
+    }
 
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isDetectingPlayer = true;
+            ShowButtonIndicator(isDetectingPlayer);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isDetectingPlayer = false;
+            ShowButtonIndicator(isDetectingPlayer);
+        }
     }
 }
