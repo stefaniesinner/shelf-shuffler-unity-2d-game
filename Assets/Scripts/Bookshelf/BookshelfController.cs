c89dd114fe275f49a792139aa7b33fb1d07deec5
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-07-21 03:25:00 +0200

    Update

--- Assets/Scripts/Bookshelf/BookshelfController.cs
+++ Assets/Scripts/Bookshelf/BookshelfController.cs
@@ -2,8 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/* Script to show how man books are left in all bookshelf sections. Therefore, it updates all book
-   sections by removing the books from the respective section
+/* Script to show how man books are left in all bookshelf sections. Therefore, it updates 
+   all book sections by removing the books from the respective section
  */
 public class BookshelfController : MonoBehaviour
 {
--- Assets/Scripts/Bookshelf/BookshelfHighlighter.cs
+++ Assets/Scripts/Bookshelf/BookshelfHighlighter.cs
@@ -5,5 +5,52 @@ using UnityEngine;
 // Script to handle the highlighting of the currently selected bookshelf section
 public class BookshelfHighlighter : MonoBehaviour
 {
+    private GameObject[] bookshelfSectionObjects;
+    private List<BookshelfManager> bookshelfSectionScripts = new List<BookshelfManager>();
+    private BookshelfManager currentBookshelfSection;
+    private int currentBookshelfSectionIndex;
+    
+    private void Start()
+    {
+        // Puts all Bookshelf Objects inside bookshelfSectionObjects
+        bookshelfSectionObjects = GameObject.FindGameObjectsWithTag("Books");
+        SetBookshelfSectionScripts();
+    }
+
+    private void Update()
+    {
+        // Checking if any sections are selected or not
+        for (int i = 0; i < bookshelfSectionObjects.Length; i++)
+        {
+            // ...
+        }
+    }
+
+    private void ResetAllHighlights()
+    {
+        for (int i = 0; i < bookshelfSectionObjects.Length; i++)
+        {
+            BookshelfSection section = bookshelfSectionObjects[i].GetComponent<BookshelfSection>();
+            //bookshelfSectionScripts[i].setVisible(bookshelfSectionScripts[i].getHighlight(), false);
+        }
+    }
+
+    private void SetBookshelfSectionScripts()
+    {
+        for (int i = 0; i < bookshelfSectionObjects.Length; i++)
+        {
+            bookshelfSectionScripts.Add(bookshelfSectionObjects[i].GetComponent<BookshelfManager>());
+        }
+    }
+
+    public BookshelfManager CurrentBookshelfSection
+    {
+        get { return currentBookshelfSection; }
+    }
+
+    public int CurrentBookshelfSectionIndex
+    {
+        get { return currentBookshelfSectionIndex; }
+    }
 
 }
