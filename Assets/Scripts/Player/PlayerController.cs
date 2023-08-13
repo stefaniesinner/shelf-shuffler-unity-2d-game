5df70dd2beaa510b856dd88e15700017d80a2f8b
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-08-13 20:44:54 +0200

    Update Scene

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,15 +8,76 @@ using UnityEngine;
  */
 public class PlayerController : MonoBehaviour
 {
+    public static PlayerController player;
+
+    private Rigidbody2D rb;
+    private Animator anim;
+    private LayerMask groundLayer;
+
+    private float speed = 5f;
+    private float jumpingPower = 5;
+    private float gravityScale;
+    private float moveHorizontal;
+    private float moveVertical;
+
+    private float groundRadius = 0.3f;
+    private float groundRayDist = 0.5f;
+
+    private bool isJumping;
+    private bool isTouchingLadder;
+    private bool isClimbing;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
+    {
+        player = GetComponent<PlayerController>();
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    private void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
         
+    }
+
+    private void Move()
+    {
+
+    }
+
+    private void Jump()
+    {
+
+    }
+
+    private bool isMoving()
+    {
+        return false;
+    }
+
+    private bool isOnGround()
+    {
+        return false;
+    }
+
+    private void FlipSprite(float xValue)
+    {
+
+    }
+
+    private void AnimatePlayer()
+    {
+
+    }
+
+    private void OnDestroy()
+    {
+        player = null;
     }
 }
