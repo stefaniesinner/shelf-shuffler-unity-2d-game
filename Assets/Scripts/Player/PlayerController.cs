7396306b07def9a29b1b78bb12c6f264f7c69614
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-08-16 22:12:07 +0200

    Update Scene

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,5 +8,43 @@ using UnityEngine;
  */
 public class PlayerController : MonoBehaviour
 {
+    private static PlayerController obj;
 
+    private bool isGrounded;
+    private bool isMoving;
+
+    private float speed = 5f;
+    private float jumpForce = 3f;
+    private float moveHorizontal;
+
+    private LayerMask groundLayer;
+    private float radius = 0.3f;
+    private float groundRayDist = 0.5f;
+
+    private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer spr;
+
+    private void Start()
+    {
+        obj = this;
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spr = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveHorizontal * speed, rb.velocity.y);
+    }
+
+    private void OnDestroy()
+    {
+        obj = null;
+    }
 }
