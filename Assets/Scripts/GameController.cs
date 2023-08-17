e93ac6cf3d4bd2bc9bf617051491a434534d9aa2
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-08-17 21:48:59 +0200

    Create Game object

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
@@ -13,12 +14,24 @@ public class GameController : MonoBehaviour
     private void Start()
     {
         obj = this;
+
+        gamePaused = false;
     }
 
     // Update is called once per frame
     private void Update()
     {
         
+    }
+
+    private void AddScore(int scoreGive)
+    {
+        score += scoreGive;
+    }
+
+    private void GameOver()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     private void OnDestroy()
