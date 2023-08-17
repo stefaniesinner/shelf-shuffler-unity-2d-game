fba5c9d4be0e1695ed84593c56b0e1969a123c68
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-08-17 21:57:39 +0200

    Update

--- Assets/Scripts/UI/Gameplay/MainGameController.cs
+++ Assets/Scripts/UI/Gameplay/MainGameController.cs
@@ -1,21 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 /**
  * Script that handles the gameplay.
  */
 public class MainGameController : MonoBehaviour
 {
+    private static MainGameController obj;
+
+    private bool gamePaused = false;
+    private int score = 0;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        obj = this;
+
+        gamePaused = false;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+
+    }
+
+    private void AddScore(int scoreGive)
+    {
+        score += scoreGive;
+    }
+
+    private void GameOver()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private void OnDestroy()
+    {
+        obj = null;
     }
 }
