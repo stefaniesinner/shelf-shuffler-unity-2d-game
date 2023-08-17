148cbb2e97c95b8fb049800b5e1d91400af69c2c
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-08-17 11:18:50 +0200

    Update

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -10,16 +10,21 @@ public class PlayerController : MonoBehaviour
 {
     private static PlayerController obj;
 
+    [SerializeField]
     private bool isGrounded;
+    [SerializeField]
     private bool isMoving;
 
     private float speed = 5f;
-    private float jumpForce = 3f;
+    private float jumpForce = 7f;
     private float moveHorizontal;
 
+    [SerializeField]
     private LayerMask groundLayer;
+    [SerializeField]
     private float radius = 0.3f;
-    private float groundRayDist = 0.5f;
+    [SerializeField]
+    private float groundRayDist = 0.55f;
 
     private Rigidbody2D rb;
     private Animator anim;
@@ -36,11 +41,48 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         moveHorizontal = Input.GetAxisRaw("Horizontal");
+
+        isMoving = (moveHorizontal != 0f);
+
+        isGrounded = Physics2D.CircleCast(transform.position, radius, Vector3.down, groundRayDist, groundLayer);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+
+        anim.SetBool("isMoving", isMoving);
+        anim.SetBool("isGrounded", isGrounded);
+
+        Flip(moveHorizontal);
     }
 
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(moveHorizontal * speed, rb.velocity.y);
+    }
+
+    private void Jump()
+    {
+        if (!isGrounded) return;
+
+        rb.velocity = Vector2.up * jumpForce;
+    }
+
+    private void Flip(float xValue)
+    {
+        Vector3 theScale = transform.localScale;
+
+        if (xValue < 0)
+        {
+            theScale.x = Mathf.Abs(theScale.x) * -1;
+        }
+        else if (xValue > 0)
+        {
+            theScale.x = Mathf.Abs(theScale.x);
+        }
+
+        transform.localScale = theScale;
     }
 
     private void OnDestroy()
