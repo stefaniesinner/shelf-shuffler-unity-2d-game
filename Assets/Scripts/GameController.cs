1c0b82734bdb442e597cfd5543fce857970d369c
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-08-17 21:38:42 +0200

    Add SceneManager

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    private static GameController obj;
+
+    private bool gamePaused = false;
+    private int score = 0;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
+    {
+        obj = this;
+    }
+
+    // Update is called once per frame
+    private void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDestroy()
     {
-        
+        obj = null;
     }
 }
