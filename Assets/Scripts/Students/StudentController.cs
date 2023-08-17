17665d5fe247289d184cd09dc1f46ff05b29e5ff
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-08-17 12:35:02 +0200

    Update

--- Assets/Scripts/Students/StudentController.cs
+++ Assets/Scripts/Students/StudentController.cs
@@ -7,15 +7,67 @@ using UnityEngine;
  */
 public class StudentController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private float moveHorizontal = 0f;
+    private float speed = 3f;
+
+    private bool isGroundFloor = true;
+    private bool isGroundFront = false;
+
+    [SerializeField]
+    private LayerMask groundLayer;
+    private float frontGroundRayDist = 0.25f;
+    private float floorCheckY = 0.52f;
+    private float frontCheck = 0.51f;
+    private float frontDist = 0.001f;
+
+    private int scoreGive = 50;
+
+    private Rigidbody2D rb;
+    private RaycastHit2D hit;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        isGroundFloor = (Physics2D.Raycast(new Vector3(transform.position.x, transform.position.y - floorCheckY, transform.position.z),
+            new Vector3(moveHorizontal, 0, 0), frontGroundRayDist, groundLayer));
+
+        if (!isGroundFloor)
+        {
+            moveHorizontal = moveHorizontal * -1;
+        }
+
+        if (Physics2D.Raycast(transform.position, new Vector3(moveHorizontal, 0, 0), frontCheck, groundLayer))
+        {
+            moveHorizontal = moveHorizontal * -1;
+        }
+
+        hit = Physics2D.Raycast(new Vector3(transform.position.x + moveHorizontal * frontCheck, transform.position.y, transform.position.z),
+            new Vector3(moveHorizontal, 0, 0), frontDist);
+
+        if (hit != null)
+        {
+            if (hit.transform != null)
+            {
+                if (hit.transform.CompareTag("Student"))
+                {
+                    moveHorizontal = moveHorizontal * -1;
+                }
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveHorizontal * speed, rb.velocity.y);
+    }
+
+    private void GetKilled()
+    {
+        gameObject.SetActive(false);
     }
 }
