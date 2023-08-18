b0798b8b4a29de23e121c20030ed7e094e90e897
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-08-18 14:49:41 +0200

    Rename BookController to InventoryController

--- Assets/Scripts/Interaction/InventoryController.cs
+++ Assets/Scripts/Interaction/InventoryController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BookController : MonoBehaviour
+public class InventoryController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
