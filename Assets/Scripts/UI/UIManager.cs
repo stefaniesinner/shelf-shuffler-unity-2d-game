a652c7a9198111b3aeee96f85546041ce8db90a8
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-08-19 19:14:31 +0200

    Update

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static UIManager obj;
+
+    public Text inventoryLbl;
+    public Text scoreLbl;
+
+    public Transform UIPanel;
+
+    private void Update()
     {
-        
+        obj = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateInventory()
     {
-        
+        inventoryLbl.text = "" + PlayerController.obj.storage;
+    }
+
+    public void UpdateScore()
+    {
+        scoreLbl.text = "" + MainGameController.obj.score;
+    }
+
+    public void StartGame()
+    {
+        AudioManager.obj.PlayJump();
+
+        MainGameController.obj.gamePaused = true;
+        UIPanel.gameObject.SetActive(true);
+    }
+
+    public void HideInitPanel()
+    {
+        AudioManager.obj.PlayJump();
+        MainGameController.obj.gamePaused = false;
+        UIPanel.gameObject.SetActive(false);
+    }
+
+    private void OnDestroy()
+    {
+        obj = null;
     }
 }
