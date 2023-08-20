a25c4731a384a95ef9e1c4b1b4f32af4c5b80254
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-08-20 20:17:43 +0200

    Add Score Update Method

--- Assets/Scripts/Interaction/InventoryController.cs
+++ Assets/Scripts/Interaction/InventoryController.cs
@@ -15,6 +15,8 @@ public class InventoryController : MonoBehaviour
 
             AudioManager.obj.PlayHit();
 
+            UIManager.obj.UpdateScore();
+
             FXManager.obj.ShowPop(transform.position);
             gameObject.SetActive(false);
         }
