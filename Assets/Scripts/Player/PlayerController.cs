f63d95cd09f9cb9908ab41247142ea1438f54a8b
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-08-26 17:06:48 +0200

    Update PlayerController.cs

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,33 +4,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public static PlayerController obj;
-
-    [SerializeField]
-    private bool isGrounded;
-    [SerializeField]
-    private bool isMoving;
-
-    public int storage = 0;
-
-    private float speed = 5f;
-    private float jumpForce = 7f;
-    private float moveHorizontal;
-
-    [SerializeField]
-    private LayerMask groundLayer;
-    [SerializeField]
-    private float radius = 0.3f;
-    [SerializeField]
-    private float groundRayDist = 0.55f;
+    public static PlayerController player;
 
     private Rigidbody2D rb;
     private Animator anim;
     private SpriteRenderer spr;
 
+    private float moveHorizontal;
+    private float speed = 5f;
+    private float jumpingPower = 7f;
+
+    [SerializeField]
+    private float groundRadius = 0.3f;
+    [SerializeField]
+    private float groundRayDist = 0.5f;
+    [SerializeField]
+    private LayerMask groundLayer;
+
+    private bool isMoving = false;
+    private bool isGrounded = false;
+
+    private void Awake()
+    {
+        player = this;
+    }
+
     private void Start()
     {
-        obj = this;
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         spr = GetComponent<SpriteRenderer>();
@@ -38,59 +38,66 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        if (GameManager.game.isPaused)
-        {
-            moveHorizontal = 0f;
-            return;
-        }
-
-        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        moveHorizontal = Input.GetAxis("Horizontal");
 
         isMoving = (moveHorizontal != 0f);
 
-        isGrounded = Physics2D.CircleCast(transform.position, radius, Vector3.down, groundRayDist, groundLayer);
+        isGrounded = Physics2D.CircleCast(transform.position, groundRadius, Vector3.down,
+            groundRayDist, groundLayer);
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
+        if (Input.GetKeyDown(KeyCode.Space)) {
             Jump();
         }
 
-        anim.SetBool("isMoving", isMoving);
-        anim.SetBool("isGrounded", isGrounded);
-
-        Flip(moveHorizontal);
+        AnimatePlayer();
+        FlipSprite(moveHorizontal);
     }
 
     private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
     {
         rb.velocity = new Vector2(moveHorizontal * speed, rb.velocity.y);
     }
 
     private void Jump()
     {
-        if (!isGrounded) return;
+        if (!isGrounded) { return; }
 
-        rb.velocity = Vector2.up * jumpForce;
+        rb.velocity = Vector2.up * jumpingPower;
     }
 
-    private void Flip(float xValue)
+    private void FlipSprite(float movementDirection)
     {
-        Vector3 theScale = transform.localScale;
+        Vector3 scaleOfObject = transform.localScale;
 
-        if (xValue < 0)
+        // move to the left
+        if (movementDirection < 0)
         {
-            theScale.x = Mathf.Abs(theScale.x) * -1;
+            // flip the object's scale
+            scaleOfObject.x = Mathf.Abs(scaleOfObject.x) * -1;
         }
-        else if (xValue > 0)
+        // move to the right
+        else if (movementDirection > 0)
         {
-            theScale.x = Mathf.Abs(theScale.x);
+            // maintain the original scale without changes
+            scaleOfObject.x = Mathf.Abs(scaleOfObject.x);
         }
 
-        transform.localScale = theScale;
+        transform.localScale = scaleOfObject;
+    }
+
+    private void AnimatePlayer()
+    {
+        anim.SetBool("isMoving", isMoving);
+        anim.SetBool("isGrounded", isGrounded);
     }
 
     private void OnDestroy()
     {
-        obj = null;
+        player = null;
     }
 }
