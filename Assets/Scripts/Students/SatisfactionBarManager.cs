bd6db9fda7de5cf1e658cc1d625a41fad9757114
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-08-27 21:40:46 +0200

    Add documentation to SatisificationBarManager.cs

--- Assets/Scripts/Students/SatisfactionBarManager.cs
+++ Assets/Scripts/Students/SatisfactionBarManager.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Class <c>SatisificationBarManager</c> handles the satisification of the students waiting for their
+/// books.
+/// </summary>
 public class SatisificationBarManager : MonoBehaviour
 {
     // Start is called before the first frame update
