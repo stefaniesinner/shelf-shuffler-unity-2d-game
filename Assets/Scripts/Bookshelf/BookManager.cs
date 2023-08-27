7d0bb895d4de12cce239d4fd2f935c05fd185838
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-08-27 21:53:40 +0200

    Updates documentation

--- Assets/Scripts/Bookshelf/BookManager.cs
+++ Assets/Scripts/Bookshelf/BookManager.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Class <c>BookManager</c> handles the books of the bookshelf. It checks from which section of the
+/// bookshelf the choosen book was selected.
+/// </summary>
 public class BookManager : MonoBehaviour
 {
     // Start is called before the first frame update
