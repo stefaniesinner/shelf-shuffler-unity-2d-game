b4471234b45ad1eb274f6c6b1b1e2d0fb203a907
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-08-27 21:39:06 +0200

    Add documentation to StudentController.cs

--- Assets/Scripts/Students/StudentController.cs
+++ Assets/Scripts/Students/StudentController.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Class <c>StudentController</c> handles the students walking into the library.
+/// </summary>
 public class StudentController : MonoBehaviour
 {
     // Start is called before the first frame update
