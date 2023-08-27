314a32943160b48f546971c2f39215e7ed58de55
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-08-27 21:07:12 +0200

    Add documentation to GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/**
- * Script that handles the game.
- */
+/// <summary>
+/// Class <c>GameManager</c> handles the game. It starts the application to let the user play the game.
+/// </summary>
 public class GameManager : MonoBehaviour
 {
     public static GameManager game;
