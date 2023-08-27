f58de2dde2692cf7af3a8884243828e1f55d0416
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-08-27 21:42:18 +0200

    Add documentation to LadderController.cs

--- Assets/Scripts/Items/LadderController.cs
+++ Assets/Scripts/Items/LadderController.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Class <c>LadderController</c> handles the ladder.
+/// </summary>
 public class LadderController : MonoBehaviour
 {
     // Start is called before the first frame update
