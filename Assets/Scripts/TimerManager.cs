f052e5b3cad6ef43578e27e161450d39e8338856
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-08-27 21:36:22 +0200

    Add documentation to TimerManager.cs

--- Assets/Scripts/TimerManager.cs
+++ Assets/Scripts/TimerManager.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Class <c>TimerManager</c> handles the timer that runs during the game and ends the game when 
+/// it expires.
+/// </summary>
 public class TimerManager : MonoBehaviour
 {
     // Start is called before the first frame update
