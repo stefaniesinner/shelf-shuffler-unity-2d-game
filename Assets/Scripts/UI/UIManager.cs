cadd033a5bc75d9822f4c35c0b54a38e4eabdd4b
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-08-27 21:37:57 +0200

    Add documentation to UIManager.cs

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Class <c>UIManager</c> handles the user interface of the game.
+/// </summary>
 public class UIManager : MonoBehaviour
 {
     // Start is called before the first frame update
