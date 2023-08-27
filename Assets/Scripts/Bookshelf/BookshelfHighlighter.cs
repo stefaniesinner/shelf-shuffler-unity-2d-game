d2962c710a148bace5077695332d4be6bfef9fd1
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-08-27 21:47:13 +0200

    Add documentation to BookshelfHighlighter.cs

--- Assets/Scripts/Bookshelf/BookshelfHighlighter.cs
+++ Assets/Scripts/Bookshelf/BookshelfHighlighter.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Class <c>BookshelfHighlighter</c> handles the highlighting of the currently selected bookshelf 
+/// section.
+/// </summary>
 public class BookshelfHighlighter : MonoBehaviour
 {
     // Start is called before the first frame update
