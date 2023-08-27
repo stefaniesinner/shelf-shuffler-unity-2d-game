a48417da444511cc9e4ee708c07b6d2a4b784e11
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-08-27 21:04:25 +0200

    Add documentation to AudioManager.cs

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Class <c>AudioManager</c> handles the audio of the game.
+/// </summary>
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager aud;
