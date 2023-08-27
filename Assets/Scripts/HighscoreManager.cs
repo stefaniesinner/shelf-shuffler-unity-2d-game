6d2fb5dd7bb7119112bd4561da3a05e4e55aced3
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-08-27 21:33:15 +0200

    Add documentation to HighscoreManager.cs

--- Assets/Scripts/HighscoreManager.cs
+++ Assets/Scripts/HighscoreManager.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Class <c>HighscoreManager</c> handles the players highscore.
+/// </summary>
 public class HighscoreManager : MonoBehaviour
 {
     // Start is called before the first frame update
