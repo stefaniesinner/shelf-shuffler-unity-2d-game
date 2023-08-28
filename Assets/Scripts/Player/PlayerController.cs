ca674b4562f172c4c84d66d06796f2c6307e7b1f
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-08-28 12:54:13 +0200

    Revert "Update PlayerController.cs"
    This reverts commit 493e81fb642422337f8cc1a0eb7a1705f1bd5419.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -25,6 +25,7 @@ public class PlayerController : MonoBehaviour
     private KeyCode jumpKey = KeyCode.Space;
 
     private bool isJumping;
+    private bool isTouchingLadder;
 
     [SerializeField]
     private float groundRadius = 0.3f;
@@ -115,6 +116,16 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    private bool IsClimbing()
+    {
+        if (isTouchingLadder && Mathf.Abs(moveVertical) > 0f)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
     /// <summary>
     /// Move to the respective direction and speed.
     /// </summary>
@@ -135,7 +146,10 @@ public class PlayerController : MonoBehaviour
 
     private void Climb()
     {
-
+        if (IsClimbing())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, moveVertical * speed);
+        }
     }
 
     /// <summary>
@@ -169,6 +183,22 @@ public class PlayerController : MonoBehaviour
         }
 
         transform.localScale = scaleOfObject;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ladder"))
+        {
+            isTouchingLadder = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ladder"))
+        {
+            isTouchingLadder = false;
+        }
     }
 
     private void OnDestroy()
