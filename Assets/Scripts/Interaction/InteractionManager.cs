5de13458a91f34d7622585c4fb01ebeef2ca665e
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-08-30 18:40:53 +0200

    Update InteractionManager.cs

--- Assets/Scripts/Interaction/InteractionManager.cs
+++ Assets/Scripts/Interaction/InteractionManager.cs
@@ -4,5 +4,23 @@ using UnityEngine;
 
 public class InteractionManager : MonoBehaviour
 {
+    public enum Interaction { NONE, GrabAndDrop }
+
+    [SerializeField]
+    private Interaction interactionType;
+
+    private KeyCode grabKey = KeyCode.G;
+
+    public void Interact()
+    {
+        if (interactionType == Interaction.GrabAndDrop)
+        {
+            if (Input.GetKeyDown(grabKey))
+            {
+                FindObjectOfType<InteractionController>().GrabAndDropItem();
+                Debug.Log("GRABBED ITEM");
+            }
+        }
+    }
 
 }
