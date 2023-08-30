aee055a8de7ae3a7f63e73533cd4432a8f46fcfe
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-08-30 18:39:51 +0200

    Update InteractionController.cs

--- Assets/Scripts/Interaction/InteractionController.cs
+++ Assets/Scripts/Interaction/InteractionController.cs
@@ -4,5 +4,69 @@ using UnityEngine;
 
 public class InteractionController : MonoBehaviour
 {
+    [SerializeField]
+    private LayerMask interactionLayer; // Only items attaching the respective Layer can be interact with the player
+    [SerializeField]
+    private GameObject interactionObject;
+    [SerializeField]
+    private Transform interactionPoint;
+    [SerializeField]
+    private float interactionRange = 0.2f;
 
+    [SerializeField]
+    private Transform grabPoint;
+    [SerializeField]
+    private GameObject grabbedItem;
+    [SerializeField]
+    private float grabbedItemYValue;
+
+    private bool isGrabbing;
+
+    private void Update()
+    {
+        Collider2D item = Physics2D.OverlapCircle(interactionPoint.position, interactionRange, interactionLayer);
+
+        if (isDetecting(item))
+        {
+            interactionObject.GetComponent<InteractionManager>().Interact();
+        }
+
+    }
+
+    private bool isDetecting(Collider2D collider)
+    {
+        if (collider != null)
+        {
+            interactionObject = collider.gameObject;
+            return true;
+        }
+
+        interactionObject = null;
+
+        return false;
+    }
+
+    public void GrabAndDropItem()
+    {
+        if (isGrabbing)
+        {
+            isGrabbing = false;
+            grabbedItem.transform.parent = null;
+            grabbedItem.transform.position = new Vector2(grabbedItem.transform.position.x, grabbedItemYValue);
+            grabbedItem = null;
+        }
+        else
+        {
+            isGrabbing = true;
+            grabbedItem = interactionObject;
+            grabbedItem.transform.parent = transform;
+            grabbedItemYValue = grabbedItem.transform.position.y;
+            grabbedItem.transform.localPosition = grabPoint.localPosition;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        interactionObject = null;
+    }
 }
