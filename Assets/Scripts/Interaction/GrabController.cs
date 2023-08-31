84de3057a47ac388e9c013e0f8f62529569e0c59
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-08-31 19:49:28 +0200

    Update

--- Assets/Scripts/Interaction/GrabController.cs
+++ Assets/Scripts/Interaction/GrabController.cs
@@ -5,5 +5,45 @@ using UnityEngine.UI;
 
 public class GrabController : MonoBehaviour
 {
+    public static GrabController grab;
 
+    [SerializeField]
+    private Transform grabPoint;
+    [SerializeField]
+    private GameObject grabbedItem;
+    [SerializeField]
+    private float grabbedItemYValue;
+
+    private bool isGrabbing;
+
+    private KeyCode grabKey = KeyCode.G;
+
+    private void Awake()
+    {
+        grab = this;
+    }
+
+    public void GrabAndDropItem()
+    {
+        if (isGrabbing)
+        {
+            isGrabbing = false;
+            grabbedItem.transform.parent = null;
+            grabbedItem.transform.position = new Vector2(grabbedItem.transform.position.x, grabbedItemYValue);
+            grabbedItem = null;
+        }
+        else
+        {
+            isGrabbing = true;
+            grabbedItem = InteractionController.controller.InteractionObject;
+            grabbedItem.transform.parent = transform;
+            grabbedItemYValue = grabbedItem.transform.position.y;
+            grabbedItem.transform.localPosition = grabPoint.localPosition;
+        }
+    }
+
+    public KeyCode GrabKey
+    {
+        get { return grabKey; }
+    }
 }
