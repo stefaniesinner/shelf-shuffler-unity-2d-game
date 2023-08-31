9451860785358b85ea4e18ec33c789de6091f88b
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-08-31 19:49:47 +0200

    Revert "Update"
    This reverts commit 7f256396efb64e559714019a4d66c62e687beb2c.

--- Assets/Scripts/Interaction/GrabController.cs
+++ Assets/Scripts/Interaction/GrabController.cs
@@ -5,5 +5,57 @@ using UnityEngine.UI;
 
 public class GrabController : MonoBehaviour
 {
+    public static GrabController grab;
 
+    [SerializeField]
+    private Transform grabPoint;
+    [SerializeField]
+    private GameObject grabTarget;
+    [SerializeField]
+    private KeyCode grabKey = KeyCode.G;
+    [SerializeField]
+    private float grabTargetYPosition;
+
+    private bool isGrabbing;
+
+    private void Awake()
+    {
+        grab = this;
+    }
+
+    private void Update()
+    {
+        if (InteractionController.controller.IsDetectingObject)
+        {
+            if (Input.GetKeyUp(grabKey))
+            {
+                Grab();
+            }
+        }
+    }
+
+    private void Grab()
+    {
+        if (isGrabbing)
+        {
+            isGrabbing = false;
+            grabTarget.transform.parent = null;
+            grabTarget.transform.position = new Vector2(grabTarget.transform.position.x,
+                grabTargetYPosition);
+            grabTarget = null;
+        }
+        else
+        {
+            isGrabbing = true;
+            grabTarget = InteractionController.controller.TargetObject;
+            grabTarget.transform.parent = transform;
+            grabTargetYPosition = grabTarget.transform.position.y;
+            grabTarget.transform.localPosition = grabPoint.localPosition;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        grab = null;
+    }
 }
--- Assets/Scripts/Interaction/InteractionController.cs
+++ Assets/Scripts/Interaction/InteractionController.cs
@@ -5,5 +5,63 @@ using UnityEngine;
 
 public class InteractionController : MonoBehaviour
 {
+    public static InteractionController controller;
 
+    [SerializeField]
+    private Transform interactionPoint;
+    [SerializeField]
+    private float interactionRange = 0.2f;
+
+    private Collider2D targetCollision;
+    [SerializeField]
+    private GameObject targetObject;
+    [SerializeField]
+    private LayerMask targetLayer;
+
+    private bool isDetectingObject;
+
+    private void Awake()
+    {
+        controller = this;
+    }
+
+    private void Update()
+    {
+        IsDetecting(targetCollision);
+    }
+
+    private void FixedUpdate()
+    {
+        targetCollision = Physics2D.OverlapCircle(interactionPoint.position,
+            interactionRange, targetLayer);
+    }
+
+    private void IsDetecting(Collider2D collider)
+    {
+        if (collider != null)
+        {
+            targetObject = collider.gameObject;
+            isDetectingObject = true;
+        }
+        else
+        {
+            targetObject = null;
+            isDetectingObject = false;
+        }
+    }
+
+    public bool IsDetectingObject
+    {
+        get { return isDetectingObject; }
+    }
+
+    public GameObject TargetObject
+    {
+        get { return targetObject; }
+    }
+
+    private void OnDestroy()
+    {
+        controller = null;
+    }
 }
