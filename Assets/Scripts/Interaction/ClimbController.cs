8b5725c31e95d0e5452a9d5f592690c2ac2a2ce7
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-09-01 18:45:39 +0200

    Update ClimbController.cs

--- Assets/Scripts/Interaction/ClimbController.cs
+++ Assets/Scripts/Interaction/ClimbController.cs
@@ -4,15 +4,77 @@ using UnityEngine;
 
 public class ClimbController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static ClimbController controller;
+
+    private Rigidbody2D rb;
+
+    private bool isTouchingLadder;
+    private bool isClimbingLadder;
+
+    private float moveVertical;
+    [SerializeField]
+    private float climbSpeed = 3f;
+
+    private void Awake()
     {
-        
+        controller = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        moveVertical = Input.GetAxisRaw("Vertical");
+    }
+
+    private void Climb()
+    {
+        if (isClimbingLadder)
+        {
+            rb.gravityScale = 0f;
+            rb.velocity = new Vector2(rb.velocity.x, moveVertical * climbSpeed);
+        }
+        else
+        {
+            rb.gravityScale = 4f;
+        }
+    }
+
+    private void IsClimbing()
+    {
+        if (isTouchingLadder && Mathf.Abs(moveVertical) > 0f)
+        {
+            isClimbingLadder = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ladder"))
+        {
+            isTouchingLadder = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ladder"))
+        {
+            isTouchingLadder = false;
+            isClimbingLadder = false;
+        }
+    }
+
+    public bool IsClimbingLadder
+    {
+        get { return isClimbingLadder; }
+    }
+
+    private void OnDestroy()
+    {
+        controller = null;
     }
 }
