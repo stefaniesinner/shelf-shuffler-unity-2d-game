c69ac990782ce321db3adfc20e3443e25b9ba7bc
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-09-02 12:20:00 +0200

    Update BookshelfHighlighter.cs

--- Assets/Scripts/Bookshelf/BookshelfHighlighter.cs
+++ Assets/Scripts/Bookshelf/BookshelfHighlighter.cs
@@ -4,15 +4,70 @@ using UnityEngine;
 
 public class BookshelfHighlighter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Array of all Bookshelf section GameObjects
+    [SerializeField]
+    private GameObject[] bookSectionObjects;
+    // List of all Bookshelf section Scripts
+    [SerializeField]
+    private List<BookshelfSectionManager> bookshelfSectionScripts = new List<BookshelfSectionManager>();
+    // The Bookshelf section that is currently selected. Can be null
+    [SerializeField]
+    private BookshelfSectionManager currentSelectedSection;
+    // The index of the current Bookshelf section. Can be null
+    private int currentBookshelfSectionIndex;
+
+    private void Start()
     {
-        
+        // Puts all Bookshelf Objects inside bookSectionObjects
+        bookSectionObjects = GameObject.FindGameObjectsWithTag("Books");
+        SetBookshelfSectionScripts();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // Checking if any sections are selected or not
+        for (int i = 0; i < bookSectionObjects.Length; i++)
+        {
+            if (bookshelfSectionScripts[i].IsSelected() == true)
+            {
+                bookshelfSectionScripts[i].SetVisible(bookshelfSectionScripts[i].Highlight, true);
+                currentSelectedSection = bookshelfSectionScripts[i];
+                currentBookshelfSectionIndex = i;
+                return;
+            }
+            else
+            {
+                bookshelfSectionScripts[i].SetVisible(bookshelfSectionScripts[i].Highlight, false);
+            }
+            currentSelectedSection = null;
+            currentBookshelfSectionIndex = -1;
+        }
+    }
+
+    private void ResetAllHighlights()
+    {
+        for (int i = 0; i < bookSectionObjects.Length; i++)
+        {
+            BookshelfSectionManager section = bookSectionObjects[i].GetComponent<BookshelfSectionManager>();
+            bookshelfSectionScripts[i].SetVisible(bookshelfSectionScripts[i].Highlight, false);
+        }
+    }
+
+    private void SetBookshelfSectionScripts()
+    {
+        for (int i = 0; i < bookSectionObjects.Length; i++)
+        {
+            bookshelfSectionScripts.Add(bookSectionObjects[i].GetComponent<BookshelfSectionManager>());
+        }
+    }
+
+    public List<BookshelfSectionManager> BookSectionScripts
+    {
+        get { return bookshelfSectionScripts; }
+    }
+
+    public int CurrentBookshelfSectionIndex
+    {
+        get { return currentBookshelfSectionIndex; }
     }
 }
