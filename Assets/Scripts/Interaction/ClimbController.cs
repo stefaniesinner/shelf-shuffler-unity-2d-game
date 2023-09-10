ae7339c053aab3396393f171e1adb19abe5113f8
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-09-10 14:39:20 +0200

    Update

--- Assets/Scripts/Interaction/ClimbController.cs
+++ Assets/Scripts/Interaction/ClimbController.cs
@@ -11,89 +11,19 @@ public class ClimbController : MonoBehaviour
 
     private Rigidbody2D rb;
 
-    private float moveVertical;
-    [SerializeField]
-    private float climbSpeed = 4f;
-
-    private bool isTouchingLadder;
-    private bool isClimbing;
-    private bool isOnLadder;
-
     private void Awake()
     {
         controller = this;
     }
 
-    private void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-
     private void Update()
     {
-        moveVertical = Input.GetAxisRaw("Vertical");
-
-        IsUsingLadder();
+        
     }
 
     private void FixedUpdate()
     {
-        Climb();
-    }
-
-    private bool IsUsingLadder()
-    {
-        if (isTouchingLadder && Mathf.Abs(moveVertical) > 0f)
-        {
-            isClimbing = true;
-            isOnLadder = true;
-        }
-        else if (Mathf.Abs(moveVertical) == 0f)
-        {
-            isOnLadder = false;
-        }
-
-        return false;
-    }
-
-    private void Climb()
-    {
-        if (isClimbing)
-        {
-            rb.gravityScale = 0f;
-            rb.velocity = new Vector2(rb.velocity.x, moveVertical * climbSpeed);
-        }
-        else
-        {
-            rb.gravityScale = 4f;
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Ladder"))
-        {
-            isTouchingLadder = true;
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Ladder"))
-        {
-            isTouchingLadder = false;
-            isClimbing = false;
-        }
-    }
-
-    public bool IsOnLadder
-    {
-        get { return isOnLadder; }
-    }
-
-    public bool IsClimbing
-    {
-        get { return isClimbing; }
+        
     }
 
     private void OnDestroy()
