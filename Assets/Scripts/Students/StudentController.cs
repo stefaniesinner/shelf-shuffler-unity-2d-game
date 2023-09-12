665fec452018b3bc74d7b69da6d9f4f45b46eeca
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-09-12 18:16:35 +0200

    Update

--- Assets/Scripts/Students/StudentController.cs
+++ Assets/Scripts/Students/StudentController.cs
@@ -8,5 +8,45 @@ using UnityEngine;
 /// </summary>
 public class StudentController : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject pointA;
+    [SerializeField]
+    private GameObject pointB;
 
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Transform currentPoint;
+
+    [SerializeField]
+    private float speed = 0f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        currentPoint = pointB.transform;
+    }
+
+    private void Update()
+    {
+        Vector2 point = currentPoint.position - transform.position;
+
+        if (currentPoint == pointB.transform)
+        {
+            rb.velocity = new Vector2(speed, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-speed, 0);
+        }
+
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform)
+        {
+            currentPoint = pointB.transform;
+        }
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointA.transform)
+        {
+            currentPoint = pointA.transform;
+        }
+    }
 }
