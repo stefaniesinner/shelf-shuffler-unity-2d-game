3c9e2222f6408e2a75fa29617895d2055565cd38
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-09-13 20:19:22 +0200

    Add HelpMenu.cs

---
+++ Assets/Scripts/UI/HelpMenu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HelpMenu : MonoBehaviour
+{
+    public void HomeMenu()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+}
