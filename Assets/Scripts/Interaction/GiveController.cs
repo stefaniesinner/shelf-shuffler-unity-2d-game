6fd3ed3347acb58001434f2e1ba7d8cba41ffd24
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-09-13 16:37:58 +0200

    Add documentary to GiveController.cs

--- Assets/Scripts/Interaction/GiveController.cs
+++ Assets/Scripts/Interaction/GiveController.cs
@@ -2,17 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Class <c>GiveController</c> handles the item exchange of two game
+/// objects.
+/// </summary>
 public class GiveController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
