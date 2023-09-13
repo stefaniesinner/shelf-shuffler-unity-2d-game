6ce40e8036d0c4dd044579c46a9e515cc23ea277
stefaniesinner <138518225+stefaniesinner@users.noreply.github.com>
2023-09-13 20:20:04 +0200

    Add EndScreen.cs

---
+++ Assets/Scripts/UI/EndScreen.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndScreen : MonoBehaviour
+{
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void QuitGame()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+
+}
